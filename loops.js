a05d7a2acdbfffe5c0520da00b8ae5c501fa5d99
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-13 18:03:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,6 @@
 function forLoop(myArray) {
-  myArray = [];
   
-  for (let i = 0; i < 25; i++) {
+    for (let i = 0; i < 25; i++) {
     if (i === 1) {
       myArray.push(`I am ${i} strange loop.`);
       
@@ -11,4 +10,12 @@ function forLoop(myArray) {
     }
   }
   return myArray;
+}
+
+function whileLoop(myNumber) {
+  while (myNumber > 0) {
+      console.log(--myNumber);
+  }
+    console.log('done');
+
 }
